71878c80f150d1d7bb5d15658d75354ab83faa79
Tim <T.Nielen@campus.lmu.de>
2024-08-07 16:16:12 +0200

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  compatibilityDate: '2024-08-07',
+  modules: ["@nuxt/ui"]
+})
