c60259d3f49295d023b1d6a44e5fd217f501598e
Tim <T.Nielen@campus.lmu.de>
2025-04-07 22:39:19 +0200

    added basic supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,5 +18,15 @@ export default defineNuxtConfig({
     head: {
       link: [{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Playpen+Sans:wght@100..800&display=swap' }]
     }
+  },
+
+  runtimeConfig: {
+    // The private keys which are only available server-side
+    // apiSecret: '123',
+    // Keys within public are also exposed client-side
+    public: {
+      SUPABASE_URL: process.env.SUPABASE_URL,
+      SUPABASE_KEY: process.env.SUPABASE_KEY,
+    }
   }
 })
--- pages/anmeldung/index.vue
+++ pages/anmeldung/index.vue
@@ -24,9 +24,12 @@
                 description="An diese E-Mail senden wir alle weiteren Informationen und die Einladung fürs nächste Jahr">
                 <UInput v-model="state.email" />
             </UFormGroup>
-            <UCheckbox label="Fitness" name="fitness"
-                help="Der/Die Teilnehmer/in ist in einer gesunden körperlichen und psychischen Verfassung und kann und darf grundsätzlich an Freizeitaktivitäten wie Wandern, Sport, Geländespiele, Schwimmbadbesuch etc. uneingeschränkt teilnehmen"
-                required :v-model="state.fitness" />
+
+            <UFormGroup label="Fitness" name="fitness" required>
+                <UCheckbox v-model="state.fitness"
+                    label="Der/Die Teilnehmer/in ist in einer gesunden körperlichen und psychischen Verfassung und kann und darf grundsätzlich an Freizeitaktivitäten wie Wandern, Sport, Geländespiele, Schwimmbadbesuch etc. uneingeschränkt teilnehmen" />
+            </UFormGroup>
+
             <UFormGroup label="Schwimmlevel" name="swimmer" required description="Der/Die Teilnehmende ist...">
                 <USelect placeholder="bitte auswählen" v-model="state.swimmer" :options="swimLevels" />
             </UFormGroup>
@@ -45,12 +48,12 @@
 
             <UFormGroup required label="Wundversorgung"
                 description="Oberflächliche Wunden dürfen mit Hilfe von handelsüblichen Desinfektionsmitteln und Wundschnellverband versorgt werden?"
-                name="woundCare">
+                name="wound_care">
                 <URadioGroup v-model="state.wound_care" :options="yesno" />
             </UFormGroup>
 
-            <UFormGroup required label="Zecken dürfen gezogen werden?" name="ticks">
-                <URadioGroup v-model="state.ticks" :options="yesno" />
+            <UFormGroup required label="Zecken dürfen gezogen werden?" name="pull_ticks">
+                <URadioGroup v-model="state.pull_ticks" :options="yesno" />
             </UFormGroup>
 
             <UFormGroup required label="Impfstatus"
@@ -65,9 +68,10 @@
                 <UTextarea v-model="state.vaccination_description" />
             </UFormGroup>
 
-            <UCheckbox label="Aufsichtspflicht: Gruppenaktivitäten" name="group_activity_consent"
-                help="Der/die Teilnehmer/in darf zusammen mit mindestens zwei weiteren Teilnehmer/innen und dem Wissen der Verantwortlichen ohne Betreuer auch manchmal alleine auf entweder bekannten oder genau gekennzeichneten Wegen gehen? Für diesen Zeitraum sind die Verantwortlichen von der Aufsichtspflicht entbunden."
-                required :v-model="state.group_activity_consent" />
+            <UFormGroup label="Aufsichtspflicht: Gruppenaktivitäten" name="group_activity_consent" required>
+                <UCheckbox v-model="state.group_activity_consent"
+                    label="Der/die Teilnehmer/in darf zusammen mit mindestens zwei weiteren Teilnehmer/innen und dem Wissen der Verantwortlichen ohne Betreuer auch manchmal alleine auf entweder bekannten oder genau gekennzeichneten Wegen gehen? Für diesen Zeitraum sind die Verantwortlichen von der Aufsichtspflicht entbunden." />
+            </UFormGroup>
 
             <UFormGroup label="Kontaktdaten Hausarzt" name="contact_doctor" required>
                 <UTextarea v-model="state.contact_doctor" />
@@ -100,24 +104,24 @@
             <UFormGroup
                 label="Wenn bei der vorangegangenen Frage 3. ausgewählt wurde, bitte hier den Namen des/der Fahrers/in angeben, bei welchem/welcher das Kind mitfährt und den Namen des zum/zur Fahrers/in gehörenden teilnehmenden Kindes."
                 name="driver">
-                <UFormGroup label="Fahrer/in Hinfahrt" name="driver_arrival">
-                    <UInput v-model="state.driver_arrival" />
+                <UFormGroup label="Fahrer/in Hinfahrt, Name des Kindes" name="arrival_driver">
+                    <UInput v-model="state.arrival_driver" />
                 </UFormGroup>
-                <UFormGroup label="Fahrer/in Rückfahrt" name="driver_return">
-                    <UInput v-model="state.driver_return" />
+                <UFormGroup label="Fahrer/in Rückfahrt, Name des Kindes" name="return_driver">
+                    <UInput v-model="state.return_driver" />
                 </UFormGroup>
             </UFormGroup>
 
             <UFormGroup
                 label="Wenn bei der vorangegangenen Frage 1. ausgewählt wurde, bitte hier angeben wie viele Plätze in dem Auto insgesamt (sprich mit dem eigenen Kind) zur Verfügung stehen:"
                 description="Alle Plätze außer dem Fahrersitz sollen angegeben werden" name="places">
-                <UFormGroup label="Hinfahrt" name="places_arrival">
-                    <USelect placeholder="bitte auswählen" v-model="state.places_arrival"
+                <UFormGroup label="Hinfahrt" name="arrival_places">
+                    <USelect placeholder="bitte auswählen" v-model="state.arrival_places"
                         :options="[0, 1, 2, 3, 4, 5]" />
                 </UFormGroup>
 
-                <UFormGroup label="Rückfahrt" name="places_return">
-                    <USelect placeholder="bitte auswählen" v-model="state.places_return"
+                <UFormGroup label="Rückfahrt" name="return_places">
+                    <USelect placeholder="bitte auswählen" v-model="state.return_places"
                         :options="[0, 1, 2, 3, 4, 5]" />
                 </UFormGroup>
             </UFormGroup>
@@ -125,12 +129,12 @@
             <UFormGroup
                 label="Wir bekommen eventuell Probleme mit der Unterbringung des gesamten Gepäcks und bitten um Umverteilung. Bzw. wir haben voraussichtlich noch ausreichen Platz für das Gepäck weiterer Kinder, welche nicht bei uns mitfahren."
                 name="baggage">
-                <UFormGroup label="Hinfahrt" name="baggage_arrival">
-                    <USelect placeholder="bitte auswählen" v-model="state.baggage_arrival" :options="baggage" />
+                <UFormGroup label="Hinfahrt" name="arrival_baggage">
+                    <USelect placeholder="bitte auswählen" v-model="state.arrival_baggage" :options="baggage" />
                 </UFormGroup>
 
-                <UFormGroup label="Rückfahrt" name="baggage_return">
-                    <USelect placeholder="bitte auswählen" v-model="state.baggage_return" :options="baggage" />
+                <UFormGroup label="Rückfahrt" name="return_baggage">
+                    <USelect placeholder="bitte auswählen" v-model="state.return_baggage" :options="baggage" />
                 </UFormGroup>
             </UFormGroup>
 
@@ -173,6 +177,7 @@
             </UFormGroup>
 
             <UFormGroup name="consent" required>
+                <UInput type="file" @change="state.consent = $event[0]" />
                 <template #label>
                     Lesen Sie nun bitte die
                     <a href="/files/08_Reisebedingungen_fur_Kirchenstiftungen_11.01.2016-1.pdf"
@@ -181,18 +186,20 @@
                     <a href="/files/Einverständniserklärung_2024.pdf" target="_blank">Einverständniserklärung</a>
                     auszufüllen und im folgenden Feld hochzuladen.
                 </template>
-                <UInput type="file" v-model="state.consent" />
             </UFormGroup>
 
-            <UCheckbox required :v-model="state.privacy_agreement" name="privacy_agreement">
-                <template #label>
-                    <span>Ich habe die
-                        <NuxtLink to="/datenschutz">Datenschutzerklärung</NuxtLink>
-                        gelesen und bin damit einverstanden, dass die angegebenen Daten vom Zeltlagerteam im Rahmen der
-                        Durchführung des diesjährigen Zeltlagers verarbeitet und gespeichert werden dürfen.
-                    </span>
-                </template>
-            </UCheckbox>
+            <UFormGroup label="Datenschutzerklärung" name="privacy_agreement" required>
+                <UCheckbox v-model="state.privacy_agreement">
+                    <template #label>
+                        <span>Ich habe die
+                            <NuxtLink to="/datenschutz">Datenschutzerklärung</NuxtLink>
+                            gelesen und bin damit einverstanden, dass die angegebenen Daten vom Zeltlagerteam im Rahmen
+                            der
+                            Durchführung des diesjährigen Zeltlagers verarbeitet und gespeichert werden dürfen.
+                        </span>
+                    </template>
+                </UCheckbox>
+            </UFormGroup>
 
             <UButton type="submit">
                 Anmeldung abschicken
@@ -204,7 +211,9 @@
 <script setup lang="ts">
 import type { FormSubmitEvent } from '#ui/types'
 import * as v from 'valibot'
-
+import { createClient } from '@supabase/supabase-js'
+const runtimeConfig = useRuntimeConfig()
+const supabase = createClient(runtimeConfig.public.SUPABASE_URL, runtimeConfig.public.SUPABASE_KEY)
 
 const genders = ['männlich', 'weiblich', 'divers'] as const;
 const swimLevels = ["Schwimmer/in", "Schwimmanfänger/in", "Nichtschwimmer/in"] as const;
@@ -218,7 +227,7 @@ const diseases = [
     "Allergien (auch Lebensmittel- und/oder Medikamentenallergie)",
     "Sonstige"
 ] as const;
-const yesno = [{ value: "Ja", label: "Ja" }, { value: "Nein", label: "Nein" }]
+const yesno = [{ value: true, label: "Ja" }, { value: false, label: "Nein" }]
 const arrival = [
     { value: "Selbst (und hat noch PLATZ frei)", label: "Ich fahre mein Kind selber und habe noch Plätze frei" },
     { value: "Selbst (und hat KEINEN platz mehr frei)", label: "Ich fahre mein Kind selber, habe aber leider KEINE Plätze mehr frei" },
@@ -248,19 +257,25 @@ const schema = v.object({
     food: v.pipe(v.string(), v.picklist(foodOptions, 'Bitte wählen Sie eine Ernährungsweise aus')),
     diseases: v.optional(v.array(v.string())),
     disease_description: v.optional(v.string()),
-    wound_care: v.pipe(v.string(), v.picklist(yesno.map(p => p.value), 'Bitte wählen Sie eine Option')),
-    ticks: v.pipe(v.string(), v.picklist(yesno.map(p => p.value), 'Bitte wählen Sie eine Option')),
-    vaccination: v.pipe(v.string(), v.picklist(yesno.map(p => p.value), 'Bitte wählen Sie eine Option')),
+    wound_care: v.boolean('Bitte wählen Sie eine Option'),
+    pull_ticks: v.boolean('Bitte wählen Sie eine Option'),
+    vaccination: v.boolean('Bitte wählen Sie eine Option'),
     vaccination_description: v.optional(v.string()),
+    contact_doctor: v.pipe(v.string(), v.minLength(2, 'Bitte geben Sie die Kontaktdaten des Hausarztes ein')),
+    arrival: v.pipe(v.string(), v.picklist(arrival.map(p => p.value), 'Bitte wählen Sie eine Option')),
     emergency_name: v.pipe(v.string(), v.minLength(2, 'Bitte geben Sie den Notfallkontakt-Namen ein')),
     emergency_relationship: v.pipe(v.string(), v.minLength(2, 'Bitte geben Sie die Beziehung an')),
     emergency_phone_number: v.pipe(v.string(), v.regex(/^\+?\d{7,15}$/, 'Bitte geben Sie eine gültige Telefonnummer ein')),
     emergency_email: v.pipe(v.string(), v.email('Bitte geben Sie eine gültige E-Mail-Adresse ein')),
     photos: v.pipe(v.string(), v.picklist(photos.map(p => p.value), 'Bitte wählen Sie eine Option aus')),
-    consent: v.pipe(v.any(), v.nonNullable('Bitte laden Sie die Einverständniserklärung hoch')),
-    fitness: v.literal(true),
-    group_activity_consent: v.literal(true),
-    privacy_agreement: v.literal(true),
+    consent: v.pipe(
+        v.file('Bitte laden Sie die Einverständniserklärung hoch'),
+        v.mimeType(['image/jpeg', 'image/png', 'application/pdf'], 'Bitte lade nur JPEG, PNG, oder PDF Dateien hoch'),
+        v.maxSize(1024 * 1024 * 5, 'Die Datei muss unter 5 MB groß sein')
+    ),
+    fitness: v.literal(true, "Bitte geben Sie uns hier Ihre Einverständnins"),
+    group_activity_consent: v.literal(true, "Bitte geben Sie uns hier Ihre Einverständnins"),
+    privacy_agreement: v.literal(true, "Bitte geben Sie uns hier Ihre Einverständnins"),
 })
 
 // Create reactive state
@@ -276,7 +291,7 @@ const state = reactive({
     diseases: [], // Can be empty or contain selected options
     disease_description: '',
     wound_care: '',
-    ticks: '',
+    pull_ticks: '',
     vaccination: '',
     vaccination_description: '',
     emergency_name: '',
@@ -284,17 +299,17 @@ const state = reactive({
     emergency_phone_number: '',
     emergency_email: '',
     photos: '',
-    consent: null, // For file upload
+    // consent: null, // For file upload
     contact_doctor: '',
 
     // Travel-related fields
     arrival: '',
-    driver_arrival: '',
-    driver_return: '',
-    places_arrival: null,
-    places_return: null,
-    baggage_arrival: '',
-    baggage_return: '',
+    arrival_driver: '',
+    arrival_places: null,
+    arrival_baggage: '',
+    return_driver: '',
+    return_places: null,
+    return_baggage: '',
 
     // Additional checkboxes
     fitness: false,
@@ -304,12 +319,36 @@ const state = reactive({
     comments: '',
 })
 
+watch(state, () => {
+    console.log(state)
+})
+
+// function test(v) {
+//     console.log(v)
+// }
 
 type Schema = v.InferOutput<typeof schema>
 const toast = useToast()
+// const supabase = useSupabaseClient()
 async function onSubmit(event: FormSubmitEvent<Schema>) {
+    console.log(event.data)
+    let payload: Record<string, any> = {}
+    for (const key in event.data) {
+        if (key != "consent" && key != "deseases") {
+            payload[key] = event.data[key]
+        }
+    }
+    console.log("test", payload)
     toast.add({ title: 'Success', description: 'The form has been submitted.', color: 'success' })
     console.log(event.data)
+    try {
+        const result = await supabase
+            .from("Registrations")
+            .insert(payload)
+        console.log(result)
+    } catch (e) {
+        console.error("error", e)
+    }
 }
 
 </script>
