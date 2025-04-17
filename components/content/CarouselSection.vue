9317fd339026415b408a305f845802a09acb7c73
Tim <T.Nielen@campus.lmu.de>
2025-04-17 18:20:56 +0200

    round

--- components/content/CarouselSection.vue
+++ components/content/CarouselSection.vue
@@ -1,8 +1,8 @@
 <template>
     <section class="w-full  xl:mx-auto py-10">
         <UCarousel v-slot="{ item }" loop :autoplay="{ delay: delay }" :items="src"
-            :ui="{ item: 'basis-1/2 lg:basis-1/3 xl:basis-1/4 ps-1 md:ps-4' }">
-            <NuxtImg :src="dir + item" class="object-cover aspect-square md:aspect-4/3 h-full w-full"
+            :ui="{ item: 'basis-1/2 lg:basis-1/3 xl:basis-1/4 ps-1 md:ps-4 ' }">
+            <NuxtImg :src="dir + item" class="object-cover aspect-square md:aspect-4/3 h-full w-full rounded-xl "
                 draggable="false" :modifiers="{ rotate: null }" format="webp" sizes="128px md:256px lg:512px" />
         </UCarousel>
     </section>
