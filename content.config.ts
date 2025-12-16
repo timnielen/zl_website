9fb710cc2b04b0752fe2f02d43000958204adf5d
Tim <tim.nielen@online.de>
2025-12-16 22:17:55 +0100

    fixed impressum/datenschutz

--- content.config.ts
+++ content.config.ts
@@ -3,7 +3,7 @@ import { defineCollection, defineContentConfig, z } from '@nuxt/content'
 export default defineContentConfig({
   collections: {
     pages: defineCollection({
-      source: '**.md',
+      source: '**/*.md',
       type: 'page'
     }),
     main: defineCollection({
